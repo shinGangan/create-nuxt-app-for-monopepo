931af8ae53997dbefbaebc2f3e473694db701652
gangan <gangan.education@gmail.com>
2024-02-02 01:36:13 +0900

    chore: add Nuxt I18n (#5)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,19 @@ export default defineNuxtConfig({
   },
   rootDir: "./src",
 
-  modules: ["@nuxt/test-utils/module", "@nuxtjs/storybook"],
+  modules: ["@nuxt/test-utils/module", "@nuxtjs/i18n", "@nuxtjs/storybook"],
+  i18n: {
+    /** @link https://v8.i18n.nuxtjs.org/guide/lazy-load-translations */
+    lazy: true,
+    langDir: "locales/",
+    defaultLocale: "ja",
+    locales: [
+      {
+        code: "ja",
+        iso: "ja-JP",
+        file: "ja.ts",
+        name: "日本語",
+      },
+    ],
+  },
 });
